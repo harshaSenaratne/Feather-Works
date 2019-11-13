d313e366cec08ec7f111a386afef529a8b6dbf51
Harsha <harsha@vizuamatix.com>
2019-11-14 02:23:05 +0530

    added sigin page redirections to logged in users

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {Switch,Route} from 'react-router-dom'
+import {Switch,Route,Redirect} from 'react-router-dom'
 import Homepage from './pages/homepage/homepage.component'
 import Shop from './pages/shop/shop.component'
 import Header from '../src/components/header/header.component'
@@ -47,7 +47,7 @@ class App extends React.Component {
    <Switch>
    <Route exact path ='/' component={Homepage}/>
    <Route  path ='/shop' component={Shop}/>
-   <Route path='/signin' component={SignInSignUp}/>
+   <Route exact path='/signin' render={()=> this.props.currentUser? (<Redirect to='/'/>) : (<SignInSignUp/>)}/>
    
    </Switch>   
    
@@ -56,8 +56,12 @@ class App extends React.Component {
  }
 }
 
+const mapStateToProps =({user}) =>({
+ currentUser : user.currentUser 
+})
+
 const mapDispatchToProps = dispatch =>({
 setCurrentUser:user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null,mapDispatchToProps)(App);
+export default connect(mapStateToProps,mapDispatchToProps)(App);
